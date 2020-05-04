0e4e0b54f086ddb08ecb7c3b0de5bc6582fed9e4
Markus Ernst <markusrolandernst@t-online.de>
2020-05-04 16:14:04 +0000

    modularizazion

---
+++ mcmc/sampler.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# Project InGen
+# Inference for Generative Computer Vision inspired by Jampani et al., 2015
+# _____________________________________________________________________________
+#
+#
+#  			  									  ____      ______         
+# May 2020   								     /  _/___  / ____/__  ____ 
+# Filename.py    							     / // __ \/ / __/ _ \/ __ \
+# Description Description                      _/ // / / / /_/ /  __/ / / /
+# Description Description                     /___/_/ /_/\____/\___/_/ /_/ 
+#                             
+# Markus Ernst  
+# 
+# _____________________________________________________________________________
+#
+#
+# Copyright 2020 Markus Ernst
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# _____________________________________________________________________________
+
+
+# ----------------
+# import libraries
+# ----------------
+
+# standard libraries
+# -----
+import numpy as np
+
+# custom functions
+# -----
+
+
+# _____________________________________________________________________________
+
+
+# -----------------
+# top-level comment
+# -----------------
+
+# medium level comment
+# -----
+
+# low level comment
